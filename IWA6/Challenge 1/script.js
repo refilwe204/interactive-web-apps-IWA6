4a31368b39aeab40ef54f17b40f7707872c9f589
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-03 14:38:26 +0200

    added comments

--- IWA6/Challenge 1/script.js
+++ IWA6/Challenge 1/script.js
@@ -3,8 +3,20 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
+/* check if the string contains of only digits
+Used the test() to check if the string contains only digits
+'^' the hat symbol is used to match the beginning of the code 
+The 'd' represents any digits
+The '+' represents one or more occurrences
+'$' represents the end of the string
+The whole expression matches string that contain one or more digits
+*/
+
 const primaryValid = /^\d+$/.test(primaryPhone)
 const secondaryValid = /^\d+$/.test(secondaryPhone)
 
+// Check if primary phone number is a valid numerical string and log the result
 console.log('Primary phone is valid numerical string:', primaryValid)
+
+// Check if secondary phone number is a valid numerical string and log the result
 console.log('Secondary phone is valid numerical string:', secondaryValid )
--- IWA6/Challenge 2/scripts.js
+++ IWA6/Challenge 2/scripts.js
@@ -8,9 +8,19 @@ const minuteOfDay = 0;
 
 // Only change below this line
 
+// Check if it's midnight and if minuteOfDay has been set
+// Also check if hourOfDay is 0 and minuteOfDay is 0
 if (hourOfDay === 0 && minuteOfDay !== null && hourOfDay == 0 && minuteOfDay == 0 ) {
+
+// Convert tax to a decimal
   const taxAsDecimal = parseInt(tax) / 100;
+
+// Calculate salary after tax
   const startingAfterTax = salary - salary * taxAsDecimal;
+
+// Calculate balance after deducting transport, food, and rent 
   const balance = startingAfterTax - transport - food - rent;
+
+// Print the balance to the console with a 'R' prefix
   console.log('R' + balance.toFixed(2));
 }
